cb942b4f67c037682f2000a086c37fccd2c1e47b
Yunseok <watemark1017@gmail.com>
2024-08-31 15:01:44 +0900

    Rename gif_botton.py -> select_gif.py

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from typing import Any
 
 import aiohttp
 import discord
+from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv()
-GUILD = discord.Object(id=os.getenv("TEST_GUILD_ID"))
-
-class GifEmojiBot(commands.AutoShardedBot):
+class EmojiBot(commands.AutoShardedBot):
     def __init__(self):
         super().__init__(
             command_prefix="!",
@@ -20,11 +18,15 @@ class GifEmojiBot(commands.AutoShardedBot):
 
     async def setup_hook(self):
         # await self.load_extension("commands.gif")
-        await self.load_extension("commands.gif_button")
+        await self.load_extension("commands.select_gif")
         await self.load_extension("commands.double")
         self.tree.copy_global_to(guild=GUILD)
         await self.tree.sync()  # guild=GUILD
-
+        
+    async def on_ready(self):
+        activity = discord.Game("/help로 명령어 보기")
+        await self.change_presence(status=discord.Status.online, activity=activity)
+    
     async def on_error(self, event_method: str, /, *args: Any, **kwargs: Any) -> None:
         print(event_method, args, kwargs)
         return await super().on_error(event_method, *args, **kwargs)
@@ -34,5 +36,9 @@ class GifEmojiBot(commands.AutoShardedBot):
         return await super().on_command_error(context, exception)
     
 if __name__=="__main__":
-    bot = GifEmojiBot()
+    load_dotenv()
+    GUILD = discord.Object(id=os.getenv("TEST_GUILD_ID"))
+    
+    bot = EmojiBot()
     bot.run(os.getenv("BOT_TOKEN"))
+    
